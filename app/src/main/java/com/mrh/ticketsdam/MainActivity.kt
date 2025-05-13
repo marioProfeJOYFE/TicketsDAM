ba2f6f675af06895bcaabd0e352c1974453e5b3a
mariorsh <mario.rios.holgado@alumnojoyfe.iepgroup.es>
2025-05-13 21:11:05 +0200

    primera pantalla

--- app/src/main/java/com/mrh/ticketsdam/MainActivity.kt
+++ app/src/main/java/com/mrh/ticketsdam/MainActivity.kt
@@ -4,20 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.mrh.ticketsdam.ui.theme.TicketsDAMTheme
@@ -54,9 +64,27 @@ class MainActivity : ComponentActivity() {
 fun ListaEventoScreen(modifier: Modifier,listaEventosViewModel: ListaEventosViewModel = viewModel()){
     val listaEventos by listaEventosViewModel.lista.collectAsStateWithLifecycle()
 
-    Column(modifier = modifier){
+    Column(modifier = modifier.fillMaxWidth().verticalScroll(rememberScrollState())){
+
+        var texto by remember { mutableStateOf("") }
+
+        TextField(
+            value = texto,
+            onValueChange = { textoEscrito ->
+                texto = textoEscrito
+            },
+            modifier = Modifier.fillMaxWidth().padding(8.dp)
+        )
         listaEventos.forEach { evento ->
-            Card {
+            Card (
+                modifier = Modifier.padding(8.dp).fillMaxWidth()
+            ){
+                Column (
+                    modifier = Modifier.padding(16.dp)
+                ){
+                    Text(text = evento.nombre, fontSize = 24.sp)
+                    Text(text = "Fecha: "+ evento.fecha.toString())
+                }
 
             }
         }
